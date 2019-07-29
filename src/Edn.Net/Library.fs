6759259eaa6ff180f74cd0f12f98b91a63e64203
Luo Tianj <lt@basecity.com>
2019-07-29 15:00:48 +0100

    reformat code

--- src/Edn.Net/Library.fs
+++ src/Edn.Net/Library.fs
@@ -1,34 +1,46 @@
 ﻿namespace Robertluo
 
-type Keyword = {
-    Ns: string option
-    Name: string
-}
+type Keyword =
+    { Ns : string option
+      Name : string }
 
-type Edn = 
-|EString of string
-| EFloat of float
-| ENull
-| EBool of bool
-| EKeyword of Keyword
-| EVector of Edn list
-| ESet of Set<Edn>
-| EMap of Map<Edn, Edn>
-
+type Edn =
+    | EString of string
+    | EFloat of float
+    | ENull
+    | EBool of bool
+    | EKeyword of Keyword
+    | EVector of Edn list
+    | ESet of Set<Edn>
+    | EMap of Map<Edn, Edn>
     override this.ToString() =
         match this with
         | ENull -> "nil"
-        | EBool v -> if v then "true" else "false"
-        | EKeyword {Ns = ns; Name = name} ->
-           match ns with
-           | Some n -> sprintf ":%s/%s" n name
-           | None -> sprintf ":%s" name
-        | EVector v -> v |> List.map (fun i -> i.ToString()) |> String.concat ", " |> sprintf "[%s]"
-        | ESet v -> Set.toList v |> List.map (fun i -> i.ToString()) |> String.concat ", " |> sprintf "#{%s}" 
-        | EMap m -> Map.toList m |> List.map (fun (k, v) -> k.ToString() + " " + v.ToString()) |> String.concat ", " |> sprintf "{%s}"
+        | EBool v ->
+            if v then "true"
+            else "false"
+        | EKeyword { Ns = ns; Name = name } ->
+            match ns with
+            | Some n -> sprintf ":%s/%s" n name
+            | None -> sprintf ":%s" name
+        | EVector v ->
+            v
+            |> List.map (fun i -> i.ToString())
+            |> String.concat ", "
+            |> sprintf "[%s]"
+        | ESet v ->
+            Set.toList v
+            |> List.map (fun i -> i.ToString())
+            |> String.concat ", "
+            |> sprintf "#{%s}"
+        | EMap m ->
+            Map.toList m
+            |> List.map (fun (k, v) -> k.ToString() + " " + v.ToString())
+            |> String.concat ", "
+            |> sprintf "{%s}"
         | EString s -> "\"" + s + "\""
         | EFloat f -> f.ToString()
- 
+
 [<RequireQualifiedAccessAttribute>]
 module Edn =
     open FParsec
@@ -42,70 +54,66 @@ module Edn =
     let enull = stringReturn "nil" ENull
 
     /// bool parser
-    let ebool = (stringReturn "true" (EBool true))
-                                    <|> (stringReturn "false" (EBool false))
+    let ebool =
+        (stringReturn "true" (EBool true))
+        <|> (stringReturn "false" (EBool false))
 
     let efloat = pfloat |>> EFloat
-
     // -------------- string ------------------------
     let str s = pstring s
-    let stringLiteral: Parser<string, unit> =
-        let escape = anyOf "\"\\/bfnrt"
-                     |>> function
-                         | 'b' -> "\b"
-                         | 'f' -> "\u000C"
-                         | 'n' -> "\n"
-                         | 'r' -> "\r"
-                         | 't' -> "\t"
-                         | c -> string c
+
+    let stringLiteral : Parser<string, unit> =
+        let escape =
+            anyOf "\"\\/bfnrt" |>> function
+            | 'b' -> "\b"
+            | 'f' -> "\u000C"
+            | 'n' -> "\n"
+            | 'r' -> "\r"
+            | 't' -> "\t"
+            | c -> string c
 
         let escapedCharSnippet = str "\\" >>. escape
         let normalCharSnippet = manySatisfy (fun c -> c <> '"' && c <> '\\')
-
         between (str "\"") (str "\"")
-                (stringsSepBy normalCharSnippet escapedCharSnippet)
+            (stringsSepBy normalCharSnippet escapedCharSnippet)
 
     let estring = stringLiteral |>> EString
-
     let comment = str ";" >>. skipRestOfLine true
-
     //--------------- keyword -----------------
-    let name = many (letter <|> anyOf "-_*?!$%&=><0123456789") |>> (List.toArray >> (System.String))
-    let ekeyword = ((str ":") >>. name .>> optional (str "/")) .>>. name 
-                                |>> (fun p ->
-                                        match p with
-                                        | (sym, "") -> EKeyword {Ns = None; Name = sym}
-                                        | (ns, sym) -> EKeyword {Ns = Some ns; Name = sym})
+    let name =
+        many (letter <|> anyOf "-_*?!$%&=><0123456789")
+        |>> (List.toArray >> (System.String))
+
+    let ekeyword =
+        ((str ":") >>. name .>> optional (str "/")) .>>. name |>> (fun p ->
+        match p with
+        | (sym, "") ->
+            EKeyword { Ns = None
+                       Name = sym }
+        | (ns, sym) ->
+            EKeyword { Ns = Some ns
+                       Name = sym })
 
     // forward declare
     let evalue, evalueRef = createParserForwardedToRef<Edn, unit>()
 
-    let ws = skipSepBy (many (anyOf " ,\t\n\r")) comment  
+    let ws =
+        skipSepBy (many (anyOf " ,\t\n\r")) comment
 
     let listBetween sopen sclose pElement f =
-        between (str sopen) (str sclose)
-                (ws >>. many (pElement .>> ws) |>> f)
-
-    let evector = listBetween "[" "]" evalue EVector           
-
+        between (str sopen) (str sclose) (ws >>. many (pElement .>> ws) |>> f)
+    let evector = listBetween "[" "]" evalue EVector
     let eset = listBetween "#{" "}" evalue (Set.ofList >> ESet)
-
     let keyValue = evalue .>>. (ws >>. evalue)
-
     let emap = listBetween "{" "}" keyValue (Map.ofList >> EMap)
 
-    do evalueRef := ws >>. choice [ebool
-                                   enull
-                                   efloat
-                                   emap
-                                   evector
-                                   eset
-                                   ekeyword
-                                   estring] .>> ws
+    do evalueRef
+       := ws
+          >>. choice
+                  [ ebool; enull; efloat; emap; evector; eset; ekeyword; estring ]
+          .>> ws
 
     //-------------- Interface ---------------
     let Parse = run evalue
-
-    let seqToString sep seq = 
+    let seqToString sep seq =
         (String.concat sep (List.map (fun i -> i.ToString()) seq))
-           
--- test/Edn.Net.Test/Tests.fs
+++ test/Edn.Net.Test/Tests.fs
@@ -6,60 +6,83 @@ open FParsec
 
 [<Tests>]
 let tests =
-  testList "Edn Parsing Test" [
-    testCase "when input is a valid edn string" <| fun _ ->
-      let matrix = dict [ ("-322.5", EFloat -322.5) 
-                          ("2554.0", EFloat 2554.0)
-                          ("false", EBool false)
-                          ("true", EBool true)
-                          ("nil", ENull)
-                          ("\"foo\"", EString "foo")
-                          (":foo/bar", EKeyword {Ns = Some("foo"); Name = "bar"})
-                          (":foo", EKeyword {Ns = None; Name = "foo"})
-                          ("#{25.0, :foo1}", ESet (Set.ofList [EFloat 25.0; EKeyword {Ns = None; Name = "foo1"}]))]
-      for KeyValue (input , expected) in matrix do
-        match Edn.Parse input with
-        | Success(actual, _, _) -> Expect.equal actual expected "should parse"
-        | _ -> failtest "not able to parse"
-
-    testCase "can skip comment" <| fun _ ->
-      let input = """
+    testList "Edn Parsing Test"
+        [ testCase "when input is a valid edn string" <| fun _ ->
+              let matrix =
+                  dict [ ("-322.5", EFloat -322.5)
+                         ("2554.0", EFloat 2554.0)
+                         ("false", EBool false)
+                         ("true", EBool true)
+                         ("nil", ENull)
+                         ("\"foo\"", EString "foo")
+                         (":foo/bar",
+                          EKeyword { Ns = Some("foo")
+                                     Name = "bar" })
+                         (":foo",
+                          EKeyword { Ns = None
+                                     Name = "foo" })
+                         ("#{25.0, :foo1}",
+                          ESet(Set.ofList [ EFloat 25.0
+                                            EKeyword { Ns = None
+                                                       Name = "foo1" } ])) ]
+              for KeyValue(input, expected) in matrix do
+                  match Edn.Parse input with
+                  | Success(actual, _, _) ->
+                      Expect.equal actual expected "should parse"
+                  | _ -> failtest "not able to parse"
+          testCase "can skip comment" <| fun _ ->
+              let input = """
                   [true, ;ok
                    nil]
                    """
-      let expected = EVector [EBool true; ENull]
-      match Edn.Parse input with
-      | Success (actual, _, _) -> Expect.equal actual expected "as white space"
-      | Failure (errorMsg, _, _) -> failtest errorMsg
 
-    testCase "can parse a nested map and other" <| fun _ ->
-      let input = """
+              let expected =
+                  EVector [ EBool true
+                            ENull ]
+              match Edn.Parse input with
+              | Success(actual, _, _) ->
+                  Expect.equal actual expected "as white space"
+              | Failure(errorMsg, _, _) -> failtest errorMsg
+          testCase "can parse a nested map and other" <| fun _ ->
+              let input = """
                   {:foo/bar [35.1, false,]
                    :a nil}
                   """
-      let expected = EMap (Map.ofList [(EKeyword {Ns = Some "foo"; Name = "bar"},
-                                        EVector [EFloat 35.1; EBool false])
-                                       (EKeyword {Ns = None; Name = "a"},
-                                        ENull)])
-      match Edn.Parse input with
-      | Success (actual, _, _) -> Expect.equal actual expected "without problem"
-      | Failure (errorMsg, _, _) -> failtest errorMsg
 
-]
+              let expected =
+                  EMap(Map.ofList [ (EKeyword { Ns = Some "foo"
+                                                Name = "bar" },
+                                     EVector [ EFloat 35.1
+                                               EBool false ])
+                                    (EKeyword { Ns = None
+                                                Name = "a" }, ENull) ])
+              match Edn.Parse input with
+              | Success(actual, _, _) ->
+                  Expect.equal actual expected "without problem"
+              | Failure(errorMsg, _, _) -> failtest errorMsg ]
 
 [<Tests>]
-let test2 = testList "ToString" [
-  testCase "keyword" <| fun _ ->
-    let input = (EKeyword {Ns = None; Name = "foo"}).ToString()
-    Expect.equal input ":foo" "should like :foo"
-    let input = (EKeyword {Ns = Some "foo"; Name = "bar"}).ToString()
-    Expect.equal input ":foo/bar" "should contains ns"
+let test2 =
+    testList "ToString"
+        [ testCase "keyword" <| fun _ ->
+              let input =
+                  (EKeyword { Ns = None
+                              Name = "foo" })
+                      .ToString()
+              Expect.equal input ":foo" "should like :foo"
+              let input =
+                  (EKeyword { Ns = Some "foo"
+                              Name = "bar" })
+                      .ToString()
+              Expect.equal input ":foo/bar" "should contains ns"
+          testCase "complex" <| fun _ ->
+              let input =
+                  EMap(Map.ofList [ (EKeyword { Ns = Some "foo"
+                                                Name = "bar" },
+                                     EVector [ EFloat 35.1
+                                               EBool false ])
+                                    (EKeyword { Ns = None
+                                                Name = "a" }, ENull) ])
 
-  testCase "complex" <| fun _ ->
-    let input =  EMap (Map.ofList [(EKeyword {Ns = Some "foo"; Name = "bar"},
-                                    EVector [EFloat 35.1; EBool false])
-                                   (EKeyword {Ns = None; Name = "a"},
-                                    ENull)])
-    let s = input.ToString()
-    Expect.equal s "{:a nil, :foo/bar [35.1, false]}" ""                                     
-]
+              let s = input.ToString()
+              Expect.equal s "{:a nil, :foo/bar [35.1, false]}" "" ]
