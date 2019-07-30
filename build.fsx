eea8d27b69811ed55617375dbef6489917f405bc
Luo Tianj <lt@basecity.com>
2019-07-30 09:49:57 +0100

    attach package info to fsproj

--- build.fsx
+++ build.fsx
@@ -16,23 +16,13 @@ let libProj = "src/Edn.Net/Edn.Net.fsproj"
 let testProj = "test/Edn.Net.Test/Edn.Net.Test.fsproj"
 
 // Targets
-Target.create "BuildApp" (fun _ ->
-  DotNet.build id libProj
-)
-
-Target.create "Test" (fun _ ->
-  DotNet.exec id "run" (sprintf "-p %s" testProj) |> ignore
-)
-
-Target.create "Pack" (fun _ ->
-  DotNet.pack id libProj
-  )
+Target.create "BuildApp" (fun _ -> DotNet.build id libProj)
+Target.create "Test"
+    (fun _ -> DotNet.exec id "run" (sprintf "-p %s" testProj) |> ignore)
+Target.create "Pack" (fun _ -> DotNet.pack id libProj)
 
 open Fake.Core.TargetOperators
 
-"BuildApp"
-  ==> "Test"
-    ==> "Pack"
-
+"BuildApp" ==> "Test" ==> "Pack"
 // start build
 Target.runOrDefault "Test"
