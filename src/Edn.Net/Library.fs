7827131153b908ac4c078f750707fc8a62d540a1
Luo Tianj <lt@basecity.com>
2019-08-16 14:02:47 +0800

    refactor ToString impl

--- src/Edn.Net/Library.fs
+++ src/Edn.Net/Library.fs
@@ -32,17 +32,15 @@ type Edn =
             if v then "true"
             else "false"
         | EString s -> 
-            let buf = System.Text.StringBuilder()
             let replaceOrLeave c =
                 match c with
-                | '\r' -> buf.Append "\\r"
-                | '\n' -> buf.Append "\\n"
-                | '\t' -> buf.Append "\\t"
-                | '\b' -> buf.Append "\\b" 
-                | '"' -> buf.Append "\\\""
-                | _ -> buf.Append c
-            s.ToCharArray() |> Array.iter (replaceOrLeave >> ignore)
-            "\"" + buf.ToString() + "\""
+                | '\r' -> "\\r"
+                | '\n' -> "\\n"
+                | '\t' -> "\\t"
+                | '\b' -> "\\b" 
+                | '"' -> "\\\""
+                | _ -> c.ToString()
+            s.ToCharArray() |> Array.map replaceOrLeave |> Array.toList |> System.String.Concat
 
         | EInteger v -> v.ToString()
         | EBigInt v -> v.ToString()
